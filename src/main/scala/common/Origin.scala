0a5da2079906facac80d9318e066b00979866535
t <toineo@users.noreply.github.com>
2015-06-08 20:40:41 -0400

    Added positions in parsing, #yolo style

--- src/main/scala/common/Origin.scala
+++ src/main/scala/common/Origin.scala
@@ -4,8 +4,8 @@ import scala.util.parsing.input.Position
 import utils._
 
 trait Origin {
-  
+
 }
-case class SourceCode(p: Position)
-case class Synthetic(creator: Str, metaorigin: Origin)
+case class SourceCode(p: Position) extends Origin
+case class Synthetic(creator: Str, metaorigin: Origin) extends Origin
 
